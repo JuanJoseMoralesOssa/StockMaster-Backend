c9c98504c0ae5bcffb8b977e61fd92be5286f6ac
juan <juanjomoss@gmail.com>
2025-07-04 16:31:26 -0500

    more

--- src/config/security.ts
+++ src/config/security.ts
@@ -1,10 +1,10 @@
 export const securityConfig = {
-  URL: process.env.BD_URL || '',
-  HOST: process.env.BD_HOST || '',
-  PORT: process.env.BD_PORT || 3306,
-  USER: process.env.BD_USER || '',
-  PASSWORD: process.env.BD_PASSWORD || '',
-  DATABASE: process.env.BD_DATABASE || '',
-  SSL_CERT_PATH: process.env.SSL_CERT_PATH || '',
-  JWT_SECRET: process.env.JWT_SECRET || '',
+  URL: process.env.BD_URL ?? '',
+  HOST: process.env.BD_HOST ?? '',
+  PORT: process.env.BD_PORT ?? 3306,
+  USER: process.env.BD_USER ?? '',
+  PASSWORD: process.env.BD_PASSWORD ?? '',
+  DATABASE: process.env.BD_DATABASE ?? '',
+  SSL_CERT_PATH: process.env.SSL_CERT_PATH ?? '',
+  JWT_SECRET: process.env.JWT_SECRET ?? '',
 };
